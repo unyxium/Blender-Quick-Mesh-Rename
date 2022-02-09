22ec535f26d37c5606827021f3cd3f5af85f96bf
Martin Ibbett <85328141+unyxium@users.noreply.github.com>
2022-02-09 16:42:36 +1100

    Add files via upload

--- quick_mesh_rename.py
+++ quick_mesh_rename.py
@@ -1,12 +1,30 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
 
 bl_info = {
     "name": "Quick Mesh Rename",
     "author": "unyxium",
     "version": (1, 0),
     "blender": (3, 0, 0),
-    "location": "Search for Quick Mesh Rename",
+    "location": "Click the mesh icon in the outliner bar.",
     "description": "Rename all mesh data blocks with the name of their parent object.",
     "category": "Mesh",
+    "doc_url": "https://github.com/unyxium/Blender-Quick-Mesh-Rename"
 }
 
 import bpy
@@ -27,15 +45,19 @@ class QuickMeshRename(bpy.types.Operator):
             
             if object.data.users == 1:
                 # object uses this mesh only
-                bpy.data.objects[object.name].data.name = object.name
-                named_meshes_count += 1
+                
+                if not bpy.data.objects[object.name].data.name == object.name:
+                    bpy.data.objects[object.name].data.name = object.name
+                    named_meshes_count += 1
             elif not object.data.name in named_meshes:
                 # object shares the mesh with others
-                bpy.data.objects[object.name].data.name = object.name
-                named_meshes_count += 1
+                
+                if not bpy.data.objects[object.name].data.name == object.name:
+                    bpy.data.objects[object.name].data.name = object.name
+                    named_meshes_count += 1
 
-                # add mesh to list so it doesn't get renamed again
-                named_meshes.append(object.data.name)
+                    # add mesh to list so it doesn't get renamed again
+                    named_meshes.append(object.data.name)
                 
         if named_meshes_count == 0:
             self.report({'INFO'}, "Renamed no meshes.")
